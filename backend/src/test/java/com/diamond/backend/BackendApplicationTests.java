00822d24b3505c1ad74ca6b634187e4c855c7238
Vidhika' Anjne <anjnevidhika@gmail.com>
2026-03-18 21:52:43 +0530

    feat() : Initialized repo

---
+++ backend/src/test/java/com/diamond/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamond.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
