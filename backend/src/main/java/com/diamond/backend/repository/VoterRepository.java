5c4504d3cdcb6c08bebeb96755089f5fb046081a
abhilasha2101 <abhilasha21012005@gmail.com>
2026-03-21 20:29:39 +0530

    ignore application.properties

--- backend/src/main/java/com/diamond/backend/repository/VoterRepository.java
+++ backend/src/main/java/com/diamond/backend/repository/VoterRepository.java
@@ -2,12 +2,13 @@ package com.diamond.backend.repository;
 
 import com.diamond.backend.model.Voter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface VoterRepository extends JpaRepository<Voter, Long> {
+public interface VoterRepository extends JpaRepository<Voter, Long>, JpaSpecificationExecutor<Voter> {
     Optional<Voter> findByVoterId(String voterId);
     Optional<Voter> findByMobileNumber(String mobileNumber);
     List<Voter> findByBoothId(String boothId);
